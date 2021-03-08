be1e8d1bae48e45bb15cd8f2334212caa306b9b6
isaidwhatisaid <78294021+isaidwhatisaid@users.noreply.github.com>
2021-03-08 22:45:53 +1100

    Update pygametutorial.py

--- pygametutorial.py
+++ pygametutorial.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 # The pygame library needs to be initialised
 pygame.init()
@@ -14,6 +15,7 @@ pygame.display.set_icon(icon)
 
 # Player
 player_img = pygame.image.load('player.png')
+player_width = 64
 player_x = 368
 player_y = 480
 player_x_change = player_y_change = 0
@@ -23,21 +25,42 @@ def player(x, y):
 
 # Grunt Enemy
 grunt_img = pygame.image.load('enemy_grunt.png')
+grunt_width = 32
 grunt_x = random.randint(10, 780)
 grunt_y = random.randint(10, 580)
 grunt_x_change = grunt_y_change = 0.1
-
-# Player function that draws the player in the right position
+# Grunt function that draws the grunt enemy type in the right position
 def grunt(x, y):
     screen.blit(grunt_img, (x, y))
 
+# Bullet
+bullet_img = pygame.image.load('bullet.png')
+bullet_width = 16
+bullet_x = player_x + 24
+bullet_y = player_y
+bullet_y_change = -0.5
+# Ready state - can't see the bullet yet
+bullet_state = 'ready'
+def fire_bullet(x, y):
+    global bullet_state
+    bullet_state = 'fire'
+    screen.blit(bullet_img, (x, y))
+
+# Collision Detection Function
+score = 0
+def is_collision(x1, y1, x2, y2):
+    distance = math.sqrt((math.pow(x1 - x2, 2)) + (math.pow(y1 - y2, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False
 
 # Game run loop
 running = True
 while running:
     
     # Fill the screen background colour
-    screen.fill((52,210,235))
+    screen.fill((52,100,235))
     
     for event in pygame.event.get():
 
@@ -51,6 +74,12 @@ while running:
                 player_y_change = -0.1
             if event.key == pygame.K_DOWN:
                 player_y_change = 0.1
+            # Fire bullet when space is pressed, so long as no other bullet is on the screen
+            if event.key == pygame.K_SPACE:
+                if bullet_state == 'ready':
+                    bullet_x = player_x + 24
+                    bullet_y = player_y
+                    fire_bullet(bullet_x, bullet_y)
         # Stop movement if key is released
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -87,6 +116,23 @@ while running:
         grunt_y_change = 0.1
     if grunt_y >= 568:
         grunt_y_change = -0.1
+    
+    # Bullet firing
+    if bullet_y <= -20:
+        bullet_state = 'ready'
+    if bullet_state == 'fire':
+        fire_bullet(bullet_x, bullet_y)
+        bullet_y += bullet_y_change
+    
+    # Collision action
+    collision = is_collision((grunt_x + grunt_width/2), (grunt_y + grunt_width/2), (bullet_x + bullet_width/2), (bullet_y + bullet_width/2))
+    if collision:
+        bullet_x = player_x + 24
+        bullet_y = player_y
+        bullet_state = 'ready'
+        score += 1
+        print(score)
+
     
     # Call the player function
     player(player_x, player_y)
