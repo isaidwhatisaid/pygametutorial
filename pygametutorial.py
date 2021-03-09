b410dae93bf78ee90fb790aed42bced424c93ff0
isaidwhatisaid <78294021+isaidwhatisaid@users.noreply.github.com>
2021-03-09 21:34:39 +1100

    Update pygametutorial.py

--- pygametutorial.py
+++ pygametutorial.py
@@ -23,15 +23,26 @@ player_x_change = player_y_change = 0
 def player(x, y):
     screen.blit(player_img, (x, y))
 
-# Grunt Enemy
-grunt_img = pygame.image.load('enemy_grunt.png')
+# Grunt Enemies
+grunt_img = []
+grunt_x = []
+grunt_y = []
+grunt_x_change = []
+grunt_y_change = []
+grunt_state = []
 grunt_width = 32
-grunt_x = random.randint(10, 780)
-grunt_y = random.randint(10, 580)
-grunt_x_change = grunt_y_change = 0.1
+num_of_grunts = 4
+for i in range(num_of_grunts):
+    grunt_img.append(pygame.image.load('enemy_grunt.png'))
+    grunt_x.append(random.randint(10, 780))
+    grunt_y.append(random.randint(10, 500))
+    grunt_x_change.append(0.07)
+    grunt_y_change.append(0.07)
+    grunt_state.append('alive')
 # Grunt function that draws the grunt enemy type in the right position
-def grunt(x, y):
-    screen.blit(grunt_img, (x, y))
+def grunt(x, y, i):
+    if grunt_state[i] == 'alive':
+        screen.blit(grunt_img[i], (x, y))
 
 # Bullet
 bullet_img = pygame.image.load('bullet.png')
@@ -50,7 +61,7 @@ def fire_bullet(x, y):
 score = 0
 def is_collision(x1, y1, x2, y2):
     distance = math.sqrt((math.pow(x1 - x2, 2)) + (math.pow(y1 - y2, 2)))
-    if distance < 27:
+    if distance < 17:
         return True
     else:
         return False
@@ -87,9 +98,12 @@ while running:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 player_y_change = 0
 
-        # Quit program condition
+        # Quit program conditions
         if event.type == pygame.QUIT:
            running = False 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
     # Move the player based on key inputs so that the player icon can be redrawn using the player function at the new location
     player_x += player_x_change
@@ -104,41 +118,44 @@ while running:
     if player_y >= 536:
         player_y = 536
 
-    # Move the grunt enemy type by bouncing them around the screen
-    grunt_x += grunt_x_change
-    grunt_y += grunt_y_change
-    # Stop grunts from moving beyond the borders
-    if grunt_x <= 0:
-        grunt_x_change = 0.1
-    if grunt_x >= 768:
-        grunt_x_change = -0.1
-    if grunt_y <= 0:
-        grunt_y_change = 0.1
-    if grunt_y >= 568:
-        grunt_y_change = -0.1
-    
     # Bullet firing
     if bullet_y <= -20:
         bullet_state = 'ready'
     if bullet_state == 'fire':
         fire_bullet(bullet_x, bullet_y)
         bullet_y += bullet_y_change
-    
-    # Collision action
-    collision = is_collision((grunt_x + grunt_width/2), (grunt_y + grunt_width/2), (bullet_x + bullet_width/2), (bullet_y + bullet_width/2))
-    if collision:
-        bullet_x = player_x + 24
-        bullet_y = player_y
-        bullet_state = 'ready'
-        score += 1
-        print(score)
 
+    # Move the grunt enemy types by bouncing them around the screen
+    for i in range(num_of_grunts):
+        # Stop grunts from moving beyond the borders
+        grunt_x[i] += grunt_x_change[i]
+        grunt_y[i] += grunt_y_change[i]          
+        if grunt_x[i] <= 0:
+            grunt_x_change[i] = 0.07
+        if grunt_x[i] >= 768:
+            grunt_x_change[i] = -0.07
+        if grunt_y[i] <= 0:
+            grunt_y_change[i] = 0.07
+        if grunt_y[i] >= 568:
+            grunt_y_change[i] = -0.07
     
+        # Collision action
+        collision = is_collision((grunt_x[i] + grunt_width/2), (grunt_y[i] + grunt_width/2), (bullet_x + bullet_width/2), (bullet_y + bullet_width/2))
+        if collision:
+            bullet_x = player_x + 24
+            bullet_y = player_y
+            bullet_state = 'ready'
+            grunt_state[i] = 'dead'
+            score += 1
+
+        # Call the enemy functions
+        grunt(grunt_x[i], grunt_y[i], i)
+   
+   
     # Call the player function
     player(player_x, player_y)
 
-    # Call the enemy functions
-    grunt(grunt_x, grunt_y)
+  
     
     
     # Update the screen with everything that's happened each loop
